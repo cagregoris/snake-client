ae863973e4eb4ba367db65bb5ae58a84efba3a52
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-07 17:01:42 -0600

    send message with name

--- client.js
+++ client.js
@@ -6,9 +6,18 @@ const connect = function() {
     host: '192.168.0.103',
     port: 50541
   });
+  // 3. register another event handler: connect event. The connect event is triggered on a connection as soon as it is successfully established.
+  conn.on('connect', function() {
+    console.log('Successfully connected to game server')
+  })
+
   conn.setEncoding('utf8');
   conn.on('data', function(message) {
     console.log(message)
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: CAG')
   })
 
   return conn;
